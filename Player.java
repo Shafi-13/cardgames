adcba059abebe6627811e223c966ac274f5ed36c
Shafi-13 <shafiuddin13071@gmail.com>
2025-07-01 20:57:04 +0100

    made player class to handle player actions and attributes that may be included in multiple games  Changes to be committed: 	new file:   Player.java

---
+++ Player.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Player{
+        private ArrayList<Card> hand;
+        private int balance;
+        public Player (int balance){
+            this.balance = balance;
+            this.hand = new ArrayList<>();
+        }
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+    public void showHand() {
+        for (Card card : hand) {
+            System.out.println(card.getName());
+        }
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+    public void draw(Card card){
+        hand.add(card);
+    }
+    public void addbalance(int amount){
+        balance += amount;
+    }
+    public void bet(int amount){
+        balance -= amount;
+    }
+    public int showbalance(){
+        return balance;
+    }
+}
