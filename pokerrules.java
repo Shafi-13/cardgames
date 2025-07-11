c256df3cbdc0ea7eeab5056d9cf10c169da90b95
Shafi-13 <shafiuddin13071@gmail.com>
2025-07-11 21:44:53 +0100

    cant believe i made progress LOOOOL  Changes to be committed: 	modified:   pokerrules.java

--- pokerrules.java
+++ pokerrules.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 class pokerrules{
@@ -23,6 +24,7 @@ class pokerrules{
         for (Card card : hand.getHand()) {
             if (!card.getSuit().equals(suit)) {
                 flush = false;
+                break;
             }
         }
         if (flush){
@@ -30,6 +32,36 @@ class pokerrules{
             Collections.sort(hand.getcardvalue());
             value = hand.getcardvalue().getLast();
             hand.setpokerhand(5);
+            hand.setscore(value);
+        }
+    }
+    public void isstraight(Player hand){
+        boolean straight = true;
+        Collections.sort(hand.getcardvalue());
+        ArrayList<Integer> lowstraight = new ArrayList<>();
+        lowstraight.add(14);
+        lowstraight.add(2);
+        lowstraight.add(3);
+        lowstraight.add(4);
+        lowstraight.add(5);
+        for (int i = 0; i < hand.getcardvalue().size() - 1; i++) {
+            if (hand.getcardvalue().get(i) + 1 != hand.getcardvalue().get(i + 1)) {
+                straight = false;
+                break;
+            }
+        }
+        if (hand.getcardvalue().equals(lowstraight)) {
+            straight = true;
+        }
+        if (straight) {
+            int value = 0;
+            for (Card card : hand.getHand()) {
+                value += card.getPokerval();
+            }
+            if (hand.getcardvalue().equals(lowstraight)) {
+                value -= 13; 
+            }
+            hand.setpokerhand(4);
             hand.setscore(value);
         }
     }
